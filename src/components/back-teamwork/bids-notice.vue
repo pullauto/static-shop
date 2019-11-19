8ae4bce5ec02026576ba511d8f61ea01aa0e7834
pullauto <1207052876@qq.com>
2019-11-19 20:03:48 +0800

    three

---
+++ src/components/back-teamwork/bids-notice.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="bids-notice">
+    <b>中标通知书</b>
+    <p>深圳森林工程有限公司</p>
+    <p>根据<b>智能工程</b>招标文件和你<b>2015-02-10</b>提交的文件，
+      回复金沙江客户发件客户缴费可垃圾分类考试缴费
+      阿放散阀困了就睡阿里九分裤垃圾
+      主要招标条件如下：
+    </p>
+
+    <div class="content-box">
+      <div class="title-left">
+        工程名称
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+.title-left{
+  
+}
+</style>
