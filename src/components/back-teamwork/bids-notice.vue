9051a9e89d2b37e0211841fc1e077cee7b8e7a28
pullauto <1207052876@qq.com>
2019-11-19 20:35:07 +0800

    4

--- src/components/back-teamwork/bids-notice.vue
+++ src/components/back-teamwork/bids-notice.vue
@@ -1,18 +1,42 @@
 <template>
   <div class="bids-notice">
-    <b>中标通知书</b>
-    <p>深圳森林工程有限公司</p>
-    <p>根据<b>智能工程</b>招标文件和你<b>2015-02-10</b>提交的文件，
-      回复金沙江客户发件客户缴费可垃圾分类考试缴费
-      阿放散阀困了就睡阿里九分裤垃圾
-      主要招标条件如下：
-    </p>
-
     <div class="content-box">
+        <b style="width: 100%; display: block; margin-top: 30px;text-align: center;">中标通知书</b>
+        <div class="table-content">
+          <b style="">深圳森林工程有限公司</b>
+          <p style="margin-top: 5px; text-indent: 2em;line-height:24px ;">根据<b>智能工程</b>招标文件和你<b>2015-02-10</b>提交的文件，
+            回复金沙江客户发件客户缴费可垃圾分类考试缴费
+            阿放散阀困了就睡阿里九分裤垃圾
+            主要招标条件如下：
+          </p>
+
+          <table border="1" width="100%">
+          <tr style="border: 1px solid #000000;">
+            <td class="table-td">Month</td>
+            <td>Savings</td>
+          </tr>
+          <tr>
+            <td class="table-td">Month</td>
+            <td>Savings</td>
+          </tr>
+          <tr>
+            <td class="table-td">Month</td>
+            <td>Savings</td>
+          </tr>
+          <tr>
+            <td class="table-td">中标工程范围</td>
+            <td>Savings</td>
+          </tr>
+          </table>
+
+        </div>
+    </div>
+
+    <!-- <div class="content-box">
       <div class="title-left">
         工程名称
       </div>
-    </div>
+    </div> -->
   </div>
 </template>
 
@@ -20,7 +44,30 @@
 </script>
 
 <style>
+.bids-notice{
+  width: 743px;
+  height: 643px;
+  background-color: #fffff5;
+  border: 1px solid #d9cba9;
+  /* padding:34px 20px 0px 20px; */
+}
+.content-box{
+  width: 713px;
+  height: 603px;
+  margin:34px 20px 0px 20px;
+  border: 1px solid #d9cba9;
+  border-top: 2px solid #ffb500;
+}
+.table-content{
+  margin: 25px 22px 0px 22px;
+}
+.table-td{
+  text-align: center;
+  width:78px;
+  height:38px;
+}
 .title-left{
-  
+  width: 78px;
+  border: ;
 }
 </style>
